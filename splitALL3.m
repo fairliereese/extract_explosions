513a92b1d9226f034c8710e3643c921fee428314
fairliereese <fairliek@comcast.net>
2016-11-10 15:26:58 -0800

    pickiness of disk directories

--- splitALL3.m
+++ splitALL3.m
@@ -2,15 +2,24 @@
 % 'btPruned', matrix - contains start and end times of each explosion
 % 'xwav1', string - path name to first xwav drive that has acoustic data
 % 'btStart', int - index to the start time of the explosion
+% 'saveFile', contains name of deployment, used to check to see if accessed
+%   disk elements are correct
 % @return
 % 'btPruned1', 'btPruned2, btPruned3', matrices - contain start and end time of each
 %   explosion, split up by where they occur within the xwav drives
-function [btPruned1, btPruned2, btPruned3] = splitALL3(btPruned, xwav1, xwav2, btStart)
+function [btPruned1, btPruned2, btPruned3] = splitALL3(btPruned, xwav1, xwav2, btStart, saveFile)
 
     % Make a list of xwavs from first drive to compare times to
     dirList = dir(fullfile(xwav1,'*disk*'));
     xwavPathAll = [];
     for iD = 1:length(dirList)
+        
+        % check to see if disk file is actually a directory we care about
+        subString = strfind(dirList(iD).name, saveFile(1:7));
+        if ~dirList(iD).isdir || isempty(subString)
+            continue
+        end
+        
         xwavNameList = dir(fullfile(xwav1,dirList(iD).name,'*x.wav'));
         xwavNameMat = vertcat(xwavNameList(:).name);
         xwavPath = fullfile(xwav1,dirList(iD).name);
@@ -42,13 +51,13 @@ function [btPruned1, btPruned2, btPruned3] = splitALL3(btPruned, xwav1, xwav2, b
     fileEnd2 = datenum(rawStart(end,:))+ (rawDur(end)/(60*60*24));
 
     % first drive's detections
-    btPruned1 = btPruned(btPruned(:, btStart) <= fileEnd1);
+    btPruned1 = btPruned(btPruned(:, btStart) <= fileEnd1, :);
 
     % second drive's detections
-    btPruned2 = btPruned(btPruned(:, btStart) > fileEnd1);
-    btPruned2 = btPruned2(btPruned2(:, btStart) <= fileEnd2);
+    btPruned2 = btPruned(btPruned(:, btStart) > fileEnd1, :);
+    btPruned2 = btPruned2(btPruned2(:, btStart) <= fileEnd2, :);
 
     % third drive's detections
-    btPruned3 = btPruned(btPruned(:, btStart) > fileEnd2);
+    btPruned3 = btPruned(btPruned(:, btStart) > fileEnd2, :);
     
 end
